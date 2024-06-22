eb05d47ea6cde4e85b557c09f7f8594cbd51abad
Rakesh Dulam <dulam.rakesh9@gmail.com>
2024-06-22 02:42:29 -0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	movies/pom.xml
    #	movies/src/main/java/com/example/movies/MovieController.java
    #	movies/src/main/java/com/example/movies/MovieService.java
    #	movies/src/main/java/com/example/movies/Review.java
    #	movies/src/main/java/com/example/movies/ReviewRepository.java

--- movies/src/main/java/com/example/movies/ReviewService.java
+++ movies/src/main/java/com/example/movies/ReviewService.java
@@ -10,13 +10,13 @@ import org.springframework.stereotype.Service;
 public class ReviewService {
 
     @Autowired
-    private ReviewRepository reviewRepository;
+    private ReviewRepository repository;
     @Autowired
     private MongoTemplate mongoTemplate;
 
     public Review createReview(String reviewBody, String imdbId) {
-        Review review = reviewRepository.insert(new Review(reviewBody));
-        reviewRepository.insert(review);
+        Review review = repository.insert(new Review(reviewBody));
+
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review))
